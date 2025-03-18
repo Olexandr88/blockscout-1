itxnetwork/blockscout

chore: Enhance health endpoint logic

apps/block_scout_web/lib/block_scout_web/controllers/api/health_controller.ex
@@ -3,12 +3,12 @@ defmodule BlockScoutWeb.API.HealthController do
 
   import Plug.Conn
 
-  alias Explorer.Chain
+  alias Explorer.Chain.Health.Helper, as: HealthHelper
   alias Explorer.Migrator.MigrationStatus
-  alias Timex.Duration
 
   @ok_message "OK"
   @backfill_multichain_search_db_migration_name "backfill_multichain_search_db"
+  @rollups [:arbitrum, :zksync, :optimism, :polygon_zkevm]
 
   @doc """
   Handles health checks for the application.
@@ -32,21 +32,48 @@ defmodule BlockScoutWeb.API.HealthController do
   end
 
   defp health(conn, _, false) do
-    with {:ok, latest_block_number_from_db, latest_block_timestamp_from_db} <- Chain.last_db_block_status(),
-         {:ok, latest_block_number_from_cache, latest_block_timestamp_from_cache} <- Chain.last_cache_block_status() do
-      send_resp(
-        conn,
-        :ok,
-        result(
-          latest_block_number_from_db,
-          latest_block_timestamp_from_db,
-          latest_block_number_from_cache,
-          latest_block_timestamp_from_cache
-        )
-      )
-    else
-      status -> send_resp(conn, :internal_server_error, encoded_error(status))
-    end
+    indexing_status = get_indexing_status()
+
+    base_health_status =
+      %{
+        metadata: %{
+          # todo: this key is left for backward compatibility
+          # and should be removed after 7.1.0 release in favour of the new health check logic based on multiple modules
+          latest_block: indexing_status.blocks.old,
+          blocks: indexing_status.blocks.new
+        }
+      }
+
+    metadata = Map.get(base_health_status, :metadata)
+
+    health_status =
+      if Application.get_env(:explorer, :chain_type) in @rollups do
+        batches_indexing_status = indexing_status.batches
+
+        base_health_status
+        |> Map.put(:metadata, Map.put(metadata, :batches, batches_indexing_status))
+        |> Map.put(:healthy, indexing_status.blocks.new.healthy and batches_indexing_status.healthy)
+      else
+        base_health_status
+        |> Map.put(:healthy, indexing_status.blocks.new.healthy)
+      end
+
+    # todo: this should be removed after 7.1.0. It is left for backward compatibility - it is artefact of the old response format.
+    blocks_property = Map.get(Map.get(health_status, :metadata), :blocks)
+
+    health_status_with_error =
+      health_status
+      |> (&if(Map.has_key?(metadata, :error),
+            do: &1,
+            else: Map.put(&1, :error, Map.get(blocks_property, :error))
+          )).()
+
+    send_resp(
+      conn,
+      :ok,
+      health_status_with_error
+      |> Jason.encode!()
+    )
   end
 
   defp health(conn, _params, true) do
@@ -101,7 +128,7 @@ defmodule BlockScoutWeb.API.HealthController do
   @spec readiness(Plug.Conn.t(), map()) :: Plug.Conn.t()
   def readiness(conn, _) do
     unless Application.get_env(:nft_media_handler, :standalone_media_worker?) do
-      Chain.last_db_block_status()
+      HealthHelper.last_db_block_status()
     end
 
     send_resp(conn, :ok, @ok_message)
@@ -146,62 +173,156 @@ defmodule BlockScoutWeb.API.HealthController do
     end
   end
 
-  defp result(
-         latest_block_number_from_db,
-         latest_block_timestamp_from_db,
-         latest_block_number_from_cache,
-         latest_block_timestamp_from_cache
-       ) do
+  @spec get_indexing_status() :: map()
+  def get_indexing_status do
+    health_status = HealthHelper.get_indexing_health_data()
+
+    blocks_old = old_blocks_indexing_status(health_status)
+    blocks_new = new_blocks_indexing_status(health_status)
+
+    common_status =
+      %{
+        blocks: %{old: blocks_old, new: blocks_new}
+      }
+
+    status =
+      if Application.get_env(:explorer, :chain_type) in @rollups do
+        batches = batches_indexing_status(health_status)
+
+        common_status
+        |> Map.put(:batches, batches)
+      else
+        common_status
+      end
+
+    status
+  end
+
+  # todo: it should be removed after 7.1.0 release in favour of the new health check logic based on multiple modules
+  defp old_blocks_indexing_status(health_status) do
+    latest_block_timestamp_from_db =
+      if is_nil(health_status[:health_latest_block_timestamp_from_db]) do
+        nil
+      else
+        {:ok, latest_block_timestamp_from_db} =
+          DateTime.from_unix(Decimal.to_integer(health_status[:health_latest_block_timestamp_from_db]))
+
+        latest_block_timestamp_from_db
+      end
+
+    latest_block_timestamp_from_cache =
+      if is_nil(health_status[:health_latest_block_timestamp_from_cache]) do
+        nil
+      else
+        {:ok, latest_block_timestamp_from_cache} =
+          DateTime.from_unix(Decimal.to_integer(health_status[:health_latest_block_timestamp_from_cache]))
+
+        latest_block_timestamp_from_cache
+      end
+
     %{
-      healthy: true,
-      metadata: %{
+      db: %{
+        number: to_string(health_status[:health_latest_block_number_from_db]),
+        timestamp: to_string(latest_block_timestamp_from_db)
+      },
+      cache: %{
+        number: to_string(health_status[:health_latest_block_number_from_cache]),
+        timestamp: to_string(latest_block_timestamp_from_cache)
+      }
+    }
+  end
+
+  defp new_blocks_indexing_status(health_status) do
+    latest_block_timestamp_from_db =
+      if is_nil(health_status[:health_latest_block_timestamp_from_db]) do
+        nil
+      else
+        {:ok, latest_block_timestamp_from_db} =
+          DateTime.from_unix(Decimal.to_integer(health_status[:health_latest_block_timestamp_from_db]))
+
+        latest_block_timestamp_from_db
+      end
+
+    latest_block_timestamp_from_cache =
+      if is_nil(health_status[:health_latest_block_timestamp_from_cache]) do
+        nil
+      else
+        {:ok, latest_block_timestamp_from_cache} =
+          DateTime.from_unix(Decimal.to_integer(health_status[:health_latest_block_timestamp_from_cache]))
+
+        latest_block_timestamp_from_cache
+      end
+
+    {healthy?, code, message} =
+      case HealthHelper.blocks_indexing_healthy?(health_status) do
+        true -> {true, 0, nil}
+        other -> other
+      end
+
+    base_response =
+      %{
+        healthy: healthy?,
         latest_block: %{
           db: %{
-            number: to_string(latest_block_number_from_db),
+            number: to_string(health_status[:health_latest_block_number_from_db]),
             timestamp: to_string(latest_block_timestamp_from_db)
           },
           cache: %{
-            number: to_string(latest_block_number_from_cache),
+            number: to_string(health_status[:health_latest_block_number_from_cache]),
             timestamp: to_string(latest_block_timestamp_from_cache)
+          },
+          node: %{
+            number: to_string(health_status[:health_latest_block_number_from_cache])
           }
         }
       }
-    }
-    |> Jason.encode!()
+
+    response =
+      if healthy? do
+        base_response
+      else
+        Map.put(base_response, :error, error(code, message))
+      end
+
+    response
   end
 
-  defp encoded_error({:error, :no_blocks}) do
-    %{
-      healthy: false,
-      error: error(5002, "There are no blocks in the DB.")
-    }
-    |> Jason.encode!()
-  end
+  defp batches_indexing_status(health_status) do
+    latest_batch_timestamp_from_db =
+      if is_nil(health_status[:health_latest_batch_timestamp_from_db]) do
+        nil
+      else
+        {:ok, latest_batch_timestamp_from_db} =
+          DateTime.from_unix(Decimal.to_integer(health_status[:health_latest_batch_timestamp_from_db]))
 
-  defp encoded_error({:stale, number, timestamp}) do
-    healthy_blocks_period = Application.get_env(:explorer, :healthy_blocks_period)
+        latest_batch_timestamp_from_db
+      end
 
-    healthy_blocks_period_minutes_formatted =
-      healthy_blocks_period
-      |> Duration.from_milliseconds()
-      |> Duration.to_minutes()
-      |> trunc()
+    {healthy?, code, message} =
+      case HealthHelper.batches_indexing_healthy?(health_status) do
+        true -> {true, 0, nil}
+        other -> other
+      end
 
-    %{
-      healthy: false,
-      error:
-        error(
-          5001,
-          "There are no new blocks in the DB for the last #{healthy_blocks_period_minutes_formatted} mins. Check the healthiness of the JSON RPC archive node or the DB."
-        ),
-      metadata: %{
-        latest_block: %{
-          number: to_string(number),
-          timestamp: to_string(timestamp)
+    base_response =
+      %{
+        healthy: healthy?,
+        latest_batch: %{
+          db: %{
+            number: to_string(health_status[:health_latest_batch_number_from_db]),
+            timestamp: to_string(latest_batch_timestamp_from_db)
+          }
         }
       }
-    }
-    |> Jason.encode!()
+
+    response =
+      if healthy? do
+        base_response
+      else
+        Map.put(base_response, :error, error(code, message))
+      end
+
+    response
   end
 
   defp error(code, message) do

apps/explorer/lib/explorer/chain/health/monitor.ex
@@ -1,0 +1,157 @@
+defmodule Explorer.Chain.Health.Monitor do
+  @moduledoc """
+  This module provides functionality for monitoring of the application health.
+  Currently, it includes monitoring of blocks and batches indexing status.
+  """
+  use GenServer
+  import Ecto.Query, only: [from: 2]
+  import EthereumJSONRPC, only: [quantity_to_integer: 1]
+  alias EthereumJSONRPC.Utility.EndpointAvailabilityChecker
+  alias Explorer.Chain.Arbitrum.Reader.Common, as: ArbitrumReaderCommon
+  alias Explorer.Chain.Health.Helper, as: HealthHelper
+  alias Explorer.Chain.ZkSync.Reader, as: ZkSyncReader
+  alias Explorer.Counters.LastFetchedCounter
+  alias Explorer.Repo
+
+  @interval :timer.seconds(5)
+
+  def start_link(_) do
+    GenServer.start_link(__MODULE__, %{}, name: __MODULE__)
+  end
+
+  @impl true
+  def init(state) do
+    schedule_work()
+    {:ok, state}
+  end
+
+  @impl true
+  def handle_info(:work, state) do
+    perform_work()
+    schedule_work()
+    {:noreply, state}
+  end
+
+  defp schedule_work do
+    Process.send_after(self(), :work, @interval)
+  end
+
+  defp perform_work do
+    json_rpc_named_arguments = Application.get_env(:explorer, :json_rpc_named_arguments)
+
+    with {latest_block_number_from_db, latest_block_timestamp_from_db} <- HealthHelper.last_db_block(),
+         {latest_block_number_from_cache, latest_block_timestamp_from_cache} <-
+           HealthHelper.last_cache_block(),
+         {:ok, latest_block_number_from_node} <-
+           EndpointAvailabilityChecker.fetch_latest_block_number(json_rpc_named_arguments) do
+      now = DateTime.utc_now()
+
+      base_params = [
+        %{
+          counter_type: "health_latest_block_number_from_db",
+          value: latest_block_number_from_db,
+          inserted_at: now,
+          updated_at: now
+        },
+        %{
+          counter_type: "health_latest_block_timestamp_from_db",
+          value: DateTime.to_unix(latest_block_timestamp_from_db),
+          inserted_at: now,
+          updated_at: now
+        },
+        %{
+          counter_type: "health_latest_block_number_from_cache",
+          value: latest_block_number_from_cache,
+          inserted_at: now,
+          updated_at: now
+        },
+        %{
+          counter_type: "health_latest_block_timestamp_from_cache",
+          value: DateTime.to_unix(latest_block_timestamp_from_cache),
+          inserted_at: now,
+          updated_at: now
+        },
+        %{
+          counter_type: "health_latest_block_number_from_node",
+          value: quantity_to_integer(latest_block_number_from_node),
+          inserted_at: now,
+          updated_at: now
+        }
+      ]
+
+      batch_info =
+        case Application.get_env(:explorer, :chain_type) do
+          :arbitrum ->
+            case ArbitrumReaderCommon.get_latest_batch_info(api?: true) do
+              {:ok,
+               %{
+                 latest_batch_number: latest_batch_number,
+                 latest_batch_timestamp: latest_batch_timestamp
+               }} ->
+                %{number: latest_batch_number, timestamp: latest_batch_timestamp}
+
+              _ ->
+                nil
+            end
+
+          :zksync ->
+            case ZkSyncReader.get_latest_batch_info(api?: true) do
+              {:ok,
+               %{
+                 latest_batch_number: latest_batch_number,
+                 latest_batch_timestamp: latest_batch_timestamp
+               }} ->
+                %{number: latest_batch_number, timestamp: latest_batch_timestamp}
+
+              _ ->
+                nil
+            end
+
+          # todo
+          # :optimism ->
+          # :polygon_zkevm ->
+
+          _ ->
+            nil
+        end
+
+      params =
+        if batch_info do
+          base_params ++
+            [
+              %{
+                counter_type: "health_latest_batch_number_from_db",
+                value: batch_info.number,
+                inserted_at: now,
+                updated_at: now
+              },
+              %{
+                counter_type: "health_latest_batch_timestamp_from_db",
+                value: DateTime.to_unix(batch_info.timestamp),
+                inserted_at: now,
+                updated_at: now
+              }
+            ]
+        else
+          base_params
+        end
+
+      Repo.insert_all(LastFetchedCounter, params,
+        on_conflict: on_conflict(),
+        conflict_target: [:counter_type]
+      )
+    end
+  end
+
+  defp on_conflict do
+    from(
+      last_fetched_counter in LastFetchedCounter,
+      update: [
+        set: [
+          value: fragment("EXCLUDED.value"),
+          updated_at: fragment("EXCLUDED.updated_at")
+        ]
+      ]
+    )
+  end
+end

Olexandr88/blockscout-1

apps/block_scout_web/lib/block_scout_web/controllers/api/health_controller.ex
@@ -3,12 +3,12 @@ defmodule BlockScoutWeb.API.HealthController do
 
   import Plug.Conn
 
-  alias Explorer.Chain
+  alias Explorer.Chain.Health.Helper, as: HealthHelper
   alias Explorer.Migrator.MigrationStatus
-  alias Timex.Duration
 
   @ok_message "OK"
   @backfill_multichain_search_db_migration_name "backfill_multichain_search_db"
+  @rollups [:arbitrum, :zksync, :optimism, :polygon_zkevm]
 
   @doc """
   Handles health checks for the application.
@@ -32,21 +32,48 @@ defmodule BlockScoutWeb.API.HealthController do
   end
 
   defp health(conn, _, false) do
-    with {:ok, latest_block_number_from_db, latest_block_timestamp_from_db} <- Chain.last_db_block_status(),
-         {:ok, latest_block_number_from_cache, latest_block_timestamp_from_cache} <- Chain.last_cache_block_status() do
-      send_resp(
-        conn,
-        :ok,
-        result(
-          latest_block_number_from_db,
-          latest_block_timestamp_from_db,
-          latest_block_number_from_cache,
-          latest_block_timestamp_from_cache
-        )
-      )
-    else
-      status -> send_resp(conn, :internal_server_error, encoded_error(status))
-    end
+    indexing_status = get_indexing_status()
+
+    base_health_status =
+      %{
+        metadata: %{
+          # todo: this key is left for backward compatibility
+          # and should be removed after 7.1.0 release in favour of the new health check logic based on multiple modules
+          latest_block: indexing_status.blocks.old,
+          blocks: indexing_status.blocks.new
+        }
+      }
+
+    metadata = Map.get(base_health_status, :metadata)
+
+    health_status =
+      if Application.get_env(:explorer, :chain_type) in @rollups do
+        batches_indexing_status = indexing_status.batches
+
+        base_health_status
+        |> Map.put(:metadata, Map.put(metadata, :batches, batches_indexing_status))
+        |> Map.put(:healthy, indexing_status.blocks.new.healthy and batches_indexing_status.healthy)
+      else
+        base_health_status
+        |> Map.put(:healthy, indexing_status.blocks.new.healthy)
+      end
+
+    # todo: this should be removed after 7.1.0. It is left for backward compatibility - it is artefact of the old response format.
+    blocks_property = Map.get(Map.get(health_status, :metadata), :blocks)
+
+    health_status_with_error =
+      health_status
+      |> (&if(Map.has_key?(metadata, :error),
+            do: &1,
+            else: Map.put(&1, :error, Map.get(blocks_property, :error))
+          )).()
+
+    send_resp(
+      conn,
+      :ok,
+      health_status_with_error
+      |> Jason.encode!()
+    )
   end
 
   defp health(conn, _params, true) do
@@ -101,7 +128,7 @@ defmodule BlockScoutWeb.API.HealthController do
   @spec readiness(Plug.Conn.t(), map()) :: Plug.Conn.t()
   def readiness(conn, _) do
     unless Application.get_env(:nft_media_handler, :standalone_media_worker?) do
-      Chain.last_db_block_status()
+      HealthHelper.last_db_block_status()
     end
 
     send_resp(conn, :ok, @ok_message)
@@ -146,62 +173,157 @@ defmodule BlockScoutWeb.API.HealthController do
     end
   end
 
-  defp result(
-         latest_block_number_from_db,
-         latest_block_timestamp_from_db,
-         latest_block_number_from_cache,
-         latest_block_timestamp_from_cache
-       ) do
+  @spec get_indexing_status() :: map()
+  def get_indexing_status do
+    health_status = HealthHelper.get_indexing_health_data()
+
+    blocks_old = old_blocks_indexing_status(health_status)
+    blocks_new = new_blocks_indexing_status(health_status)
+
+    common_status =
+      %{
+        blocks: %{old: blocks_old, new: blocks_new}
+      }
+
+    status =
+      if Application.get_env(:explorer, :chain_type) in @rollups do
+        batches = batches_indexing_status(health_status)
+
+        common_status
+        |> Map.put(:batches, batches)
+      else
+        common_status
+      end
+
+    status
+  end
+
+  # todo: it should be removed after 7.1.0 release in favour of the new health check logic based on multiple modules
+  defp old_blocks_indexing_status(health_status) do
+    latest_block_timestamp_from_db =
+      if is_nil(health_status[:health_latest_block_timestamp_from_db]) do
+        nil
+      else
+        {:ok, latest_block_timestamp_from_db} =
+          DateTime.from_unix(Decimal.to_integer(health_status[:health_latest_block_timestamp_from_db]))
+
+        latest_block_timestamp_from_db
+      end
+
+    latest_block_timestamp_from_cache =
+      if is_nil(health_status[:health_latest_block_timestamp_from_cache]) do
+        nil
+      else
+        {:ok, latest_block_timestamp_from_cache} =
+          DateTime.from_unix(Decimal.to_integer(health_status[:health_latest_block_timestamp_from_cache]))
+
+        latest_block_timestamp_from_cache
+      end
+
     %{
-      healthy: true,
-      metadata: %{
+      db: %{
+        number: to_string(health_status[:health_latest_block_number_from_db]),
+        timestamp: to_string(latest_block_timestamp_from_db)
+      },
+      cache: %{
+        number: to_string(health_status[:health_latest_block_number_from_cache]),
+        timestamp: to_string(latest_block_timestamp_from_cache)
+      }
+    }
+  end
+
+  defp new_blocks_indexing_status(health_status) do
+    latest_block_timestamp_from_db =
+      if is_nil(health_status[:health_latest_block_timestamp_from_db]) do
+        nil
+      else
+        {:ok, latest_block_timestamp_from_db} =
+          DateTime.from_unix(Decimal.to_integer(health_status[:health_latest_block_timestamp_from_db]))
+
+        latest_block_timestamp_from_db
+      end
+
+    latest_block_timestamp_from_cache =
+      if is_nil(health_status[:health_latest_block_timestamp_from_cache]) do
+        nil
+      else
+        {:ok, latest_block_timestamp_from_cache} =
+          DateTime.from_unix(Decimal.to_integer(health_status[:health_latest_block_timestamp_from_cache]))
+
+        latest_block_timestamp_from_cache
+      end
+
+    {healthy?, code, message} =
+      case HealthHelper.blocks_indexing_healthy?(health_status) do
+        true -> {true, 0, nil}
+        other -> other
+      end
+
+    base_response =
+      %{
+        healthy: healthy?,
         latest_block: %{
           db: %{
-            number: to_string(latest_block_number_from_db),
+            number: to_string(health_status[:health_latest_block_number_from_db]),
             timestamp: to_string(latest_block_timestamp_from_db)
           },
           cache: %{
-            number: to_string(latest_block_number_from_cache),
+            number: to_string(health_status[:health_latest_block_number_from_cache]),
             timestamp: to_string(latest_block_timestamp_from_cache)
+          },
+          node: %{
+            number: to_string(health_status[:health_latest_block_number_from_node])
           }
         }
       }
-    }
-    |> Jason.encode!()
+
+    response =
+      if healthy? do
+        base_response
+      else
+        Map.put(base_response, :error, error(code, message))
+      end
+
+    response
   end
 
-  defp encoded_error({:error, :no_blocks}) do
-    %{
-      healthy: false,
-      error: error(5002, "There are no blocks in the DB.")
-    }
-    |> Jason.encode!()
-  end
+  defp batches_indexing_status(health_status) do
+    latest_batch_timestamp_from_db =
+      if is_nil(health_status[:health_latest_batch_timestamp_from_db]) do
+        nil
+      else
+        {:ok, latest_batch_timestamp_from_db} =
+          DateTime.from_unix(Decimal.to_integer(health_status[:health_latest_batch_timestamp_from_db]))
 
-  defp encoded_error({:stale, number, timestamp}) do
-    healthy_blocks_period = Application.get_env(:explorer, :healthy_blocks_period)
+        latest_batch_timestamp_from_db
+      end
 
-    healthy_blocks_period_minutes_formatted =
-      healthy_blocks_period
-      |> Duration.from_milliseconds()
-      |> Duration.to_minutes()
-      |> trunc()
+    {healthy?, code, message} =
+      case HealthHelper.batches_indexing_healthy?(health_status) do
+        true -> {true, 0, nil}
+        other -> other
+      end
 
-    %{
-      healthy: false,
-      error:
-        error(
-          5001,
-          "There are no new blocks in the DB for the last #{healthy_blocks_period_minutes_formatted} mins. Check the healthiness of the JSON RPC archive node or the DB."
-        ),
-      metadata: %{
-        latest_block: %{
-          number: to_string(number),
-          timestamp: to_string(timestamp)
+    base_response =
+      %{
+        healthy: healthy?,
+        latest_batch: %{
+          db: %{
+            number: to_string(health_status[:health_latest_batch_number_from_db]),
+            timestamp: to_string(latest_batch_timestamp_from_db),
+            average_batch_time: to_string(health_status[:health_latest_batch_average_time_from_db])
+          }
         }
       }
-    }
-    |> Jason.encode!()
+
+    response =
+      if healthy? do
+        base_response
+      else
+        Map.put(base_response, :error, error(code, message))
+      end
+
+    response
   end
 
   defp error(code, message) do

apps/explorer/lib/explorer/chain/health/monitor.ex
@@ -1,0 +1,173 @@
+defmodule Explorer.Chain.Health.Monitor do
+  @moduledoc """
+  This module provides functionality for monitoring of the application health.
+  Currently, it includes monitoring of blocks and batches indexing status.
+  """
+  use GenServer
+  import Ecto.Query, only: [from: 2]
+  import EthereumJSONRPC, only: [quantity_to_integer: 1]
+  alias EthereumJSONRPC.Utility.EndpointAvailabilityChecker
+  alias Explorer.Chain.Arbitrum.Reader.Common, as: ArbitrumReaderCommon
+  alias Explorer.Chain.Health.Helper, as: HealthHelper
+  alias Explorer.Chain.ZkSync.Reader, as: ZkSyncReader
+  alias Explorer.Counters.LastFetchedCounter
+  alias Explorer.Repo
+
+  @interval :timer.minutes(5)
+
+  def start_link(_) do
+    GenServer.start_link(__MODULE__, %{}, name: __MODULE__)
+  end
+
+  @impl true
+  def init(state) do
+    schedule_work()
+    {:ok, state}
+  end
+
+  @impl true
+  def handle_info(:work, state) do
+    perform_work()
+    schedule_work()
+    {:noreply, state}
+  end
+
+  defp schedule_work do
+    Process.send_after(self(), :work, Application.get_env(:explorer, __MODULE__)[:check_interval] || @interval)
+  end
+
+  defp perform_work do
+    json_rpc_named_arguments = Application.get_env(:explorer, :json_rpc_named_arguments)
+
+    with {latest_block_number_from_db, latest_block_timestamp_from_db} <- HealthHelper.last_db_block(),
+         {latest_block_number_from_cache, latest_block_timestamp_from_cache} <-
+           HealthHelper.last_cache_block(),
+         {:ok, latest_block_number_from_node} <-
+           EndpointAvailabilityChecker.fetch_latest_block_number(json_rpc_named_arguments) do
+      now = DateTime.utc_now()
+
+      base_params = [
+        %{
+          counter_type: "health_latest_block_number_from_db",
+          value: latest_block_number_from_db,
+          inserted_at: now,
+          updated_at: now
+        },
+        %{
+          counter_type: "health_latest_block_timestamp_from_db",
+          value: DateTime.to_unix(latest_block_timestamp_from_db),
+          inserted_at: now,
+          updated_at: now
+        },
+        %{
+          counter_type: "health_latest_block_number_from_cache",
+          value: latest_block_number_from_cache,
+          inserted_at: now,
+          updated_at: now
+        },
+        %{
+          counter_type: "health_latest_block_timestamp_from_cache",
+          value: DateTime.to_unix(latest_block_timestamp_from_cache),
+          inserted_at: now,
+          updated_at: now
+        },
+        %{
+          counter_type: "health_latest_block_number_from_node",
+          value: quantity_to_integer(latest_block_number_from_node),
+          inserted_at: now,
+          updated_at: now
+        }
+      ]
+
+      batch_info =
+        case Application.get_env(:explorer, :chain_type) do
+          :arbitrum ->
+            case ArbitrumReaderCommon.get_latest_batch_info(api?: true) do
+              {:ok,
+               %{
+                 latest_batch_number: latest_batch_number,
+                 latest_batch_timestamp: latest_batch_timestamp,
+                 average_batch_time: average_batch_time
+               }} ->
+                %{
+                  number: latest_batch_number,
+                  timestamp: latest_batch_timestamp,
+                  average_batch_time: average_batch_time
+                }
+
+              _ ->
+                nil
+            end
+
+          :zksync ->
+            case ZkSyncReader.get_latest_batch_info(api?: true) do
+              {:ok,
+               %{
+                 latest_batch_number: latest_batch_number,
+                 latest_batch_timestamp: latest_batch_timestamp,
+                 average_batch_time: average_batch_time
+               }} ->
+                %{
+                  number: latest_batch_number,
+                  timestamp: latest_batch_timestamp,
+                  average_batch_time: average_batch_time
+                }
+
+              _ ->
+                nil
+            end
+
+          # todo
+          # :optimism ->
+          # :polygon_zkevm ->
+
+          _ ->
+            nil
+        end
+
+      params =
+        if batch_info do
+          base_params ++
+            [
+              %{
+                counter_type: "health_latest_batch_number_from_db",
+                value: batch_info.number,
+                inserted_at: now,
+                updated_at: now
+              },
+              %{
+                counter_type: "health_latest_batch_timestamp_from_db",
+                value: DateTime.to_unix(batch_info.timestamp),
+                inserted_at: now,
+                updated_at: now
+              },
+              %{
+                counter_type: "health_latest_batch_average_time_from_db",
+                value: batch_info.average_batch_time,
+                inserted_at: now,
+                updated_at: now
+              }
+            ]
+        else
+          base_params
+        end
+
+      Repo.insert_all(LastFetchedCounter, params,
+        on_conflict: on_conflict(),
+        conflict_target: [:counter_type]
+      )
+    end
+  end
+
+  defp on_conflict do
+    from(
+      last_fetched_counter in LastFetchedCounter,
+      update: [
+        set: [
+          value: fragment("EXCLUDED.value"),
+          updated_at: fragment("EXCLUDED.updated_at")
+        ]
+      ]
+    )
+  end
+end